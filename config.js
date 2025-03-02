e9f69d2e4ce1f0fa0de62ac57a2d8f419599c9a7
KannamTejaswi311 <kannamtejaswi1341@gmail.com>
2025-03-02 10:11:59 +0530

    Initial commit

---
+++ config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://lisa:lisa@book-store-mern.2cbjr.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-store-MERN';
